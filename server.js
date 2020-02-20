6f12253509f66b67dcfabb91a0d01f76adb0ec6a
Mike Corbett <srcorbetto@gmail.com>
2020-02-20 14:40:12 -0600

    Updated A-Frame version

--- server.js
+++ server.js
@@ -1,28 +1,31 @@
-require('dotenv').config({path: __dirname + '/.env'})
+require("dotenv").config({ path: __dirname + "/.env" });
 
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const app = express();
-const path = require('path');
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/' + '/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/" + "/index.html"));
 });
 
-app.get('/weather', (req, res) => {
-    console.log('req:', req.query.id);
-    axios.get(`http://api.openweathermap.org/data/2.5/weather?id=${req.query.id}&units=imperial&APPID=${process.env.API_KEY}`)
+app.get("/weather", (req, res) => {
+  console.log("req:", req.query.id);
+  axios
+    .get(
+      `http://api.openweathermap.org/data/2.5/weather?id=${req.query.id}&units=imperial&APPID=${process.env.API_KEY}`
+    )
     .then(response => {
-        console.log(response.data);
+      console.log(response.data);
 
-        res.send(response.data);
+      res.send(response.data);
     })
     .catch(error => {
-        console.log(error);
+      console.log(error);
     });
 });
 
